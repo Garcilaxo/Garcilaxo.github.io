fc2a3d586afb26aa5706ea0b568f39baae355446
Garcilaxo <alejandrogaf91@gmail.com>
2023-09-11 17:45:58 +0200

    Ficha

--- js/Swal.js
+++ js/Swal.js
@@ -1,12 +1,15 @@
 function Loading() {
     Swal.showLoading();
 
+    var cargarFicha = document.getElementById("cargarFicha");
+
     // Aquí puedes realizar alguna operación asincrónica, como cargar datos o realizar una solicitud AJAX
     // Después de que la operación asincrónica haya finalizado, puedes ocultar el mensaje de carga con Swal.close()
 
-    // Ejemplo de cierre después de 3 segundos (simulación)
-    setTimeout(function() {
-        Swal.close();
-        // Puedes agregar aquí el código para mostrar el contenido deseado después de cargar
-    }, 3000); // Cambia el tiempo a tu preferencia
+    cargarFicha.addEventListener("click", function () {
+        // Redirige al usuario a "nueva_pagina.html"
+        window.location.href = "Ficha.html";
+    });
+
+    Swal.close();
 }
