a2d97cac5da23832e257934a3c3259e7a6f9f8e7
Garcilaxo <alejandrogaf91@gmail.com>
2023-09-12 14:26:39 +0200

    act

--- js/app.js
+++ js/app.js
@@ -8,8 +8,10 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById("nombre").textContent = `${data.influcard.username}`;
       document.getElementById("imagen").src = data.influcard.account_picture;
       document.getElementById("link").href = data.influcard.account_url;
-      document.getElementById("edad").textContent = ` Mujer, ${data.influcard.age} años`;
-  } else {
+      document.getElementById("edad").textContent = `Mujer, ${data.influcard.age} años`;
+  } 
+  
+  else {
       console.error("No se encontraron datos en el almacenamiento local.");
   }
 });
