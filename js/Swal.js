b661b9591f216f75271d6457ae4e662e0acd8b02
Garcilaxo <alejandrogaf91@gmail.com>
2023-09-12 13:31:26 +0200

    act

--- js/Swal.js
+++ js/Swal.js
@@ -19,24 +19,7 @@ function Loading() {
         console.error("Error:", error);
     });
 
-    CargarDatos();
-
     Swal.close();
 }
 
-function CargarDatos() {
-    // Leer los datos del almacenamiento local
-    const dataJSON = localStorage.getItem("datosJSON");
-    if (dataJSON) {
-        const data = JSON.parse(dataJSON);
-        window.addEventListener('load', function(){
-            console.log(data.influcard.username);
-            // Mostrar los datos en la página
-            document.getElementById("nombre").textContent = data.influcard.username;
-            //document.getElementById("edad").textContent = `Edad: ${data.edad}`;
-            //document.getElementById("ciudad").textContent = `Ciudad: ${data.ciudad}`;
-        });
-    } else {
-        console.error("No se encontraron datos en el almacenamiento local.");
-    }
-  }
+
--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
-/*
-function CargarDatos() {
+document.addEventListener("DOMContentLoaded", function() {
   // Leer los datos del almacenamiento local
   const dataJSON = localStorage.getItem("datosJSON");
   if (dataJSON) {
       const data = JSON.parse(dataJSON);
 
+      console.log(data.influcard.username);
       // Mostrar los datos en la página
-      document.getElementById("nombre").textContent = data.influcard.username;
+      document.getElementById("nombre").textContent = `Nombre: ${data.influcard.username}`;
       //document.getElementById("edad").textContent = `Edad: ${data.edad}`;
       //document.getElementById("ciudad").textContent = `Ciudad: ${data.ciudad}`;
   } else {
       console.error("No se encontraron datos en el almacenamiento local.");
   }
-}
-*/
+});
