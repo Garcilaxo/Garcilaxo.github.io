5c5a1cf8cd5a8bf57dfaf865d5d21cf0ba10842f
Garcilaxo <alejandrogaf91@gmail.com>
2023-09-13 00:25:13 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,19 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Leer los datos del almacenamiento local
+  // Leo los datos del almacenamiento local
   const dataJSON = localStorage.getItem("datosJSON");
   if (dataJSON) {
       const data = JSON.parse(dataJSON);
       
-      // Mostrar los datos en la página
+      // Muestro los datos en la página
       document.getElementById("nombre").textContent = `${data.influcard.username}`;
       document.getElementById("imagen").src = data.influcard.account_picture;
       document.getElementById("link").href = data.influcard.account_url;
       document.getElementById("edad").textContent = `Mujer, ${data.influcard.age} años`;
       document.getElementById("date").textContent = `Datos actualizados a ${data.influcard.updated_at_formated}`;
       document.getElementById("followers").textContent = `${data.influcard.followers_formated}`;
-      document.getElementById("fake_followers").textContent = `${data.influcard.fake_followers_formated} K`;
+      document.getElementById("fake_followers").textContent = `${data.influcard.fake_followers_formated} %`;
       document.getElementById("real_followers").textContent = `${data.influcard.real_followers_formated}`;
+      document.getElementById("alcance").textContent = `${data.influcard.reach_formated}`;
+      document.getElementById("impresiones").textContent = `${data.influcard.avg_impressions_formated}`;
+      document.getElementById("reproducciones").textContent = `${data.influcard.vplays_formated}`;
+      document.getElementById("engagement").textContent = `${data.influcard.avg_engagement_formated}`;
       
+      
+
+    
   } 
   
   else {
