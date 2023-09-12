69f38f44d3731e92c84125c5317c98ccc159e03c
Garcilaxo <alejandrogaf91@gmail.com>
2023-09-12 11:53:41 +0200

    Update

--- js/Swal.js
+++ js/Swal.js
@@ -5,22 +5,23 @@ function Loading() {
     // Aquí puedes realizar alguna operación asincrónica, como cargar datos o realizar una solicitud AJAX
     // Después de que la operación asincrónica haya finalizado, puedes ocultar el mensaje de carga con Swal.close()
 
-    fetch('json/data.json')
-        .then(function (response) {
+    fetch("datos.json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error al cargar el archivo JSON: ${response.status}`);
+            }
             return response.json();
         })
-        .then(function (data) {
-            // Mostrar los datos en el HTML
-            document.getElementById("nombre").textContent = data.username;
-            //document.getElementById("edad").textContent = data.edad;
-            //document.getElementById("ciudad").textContent = data.ciudad;
-            console.log(document.getElementById("nombre").textContent);
+        .then(data => {
+            // Almacenar los datos en el almacenamiento local (localStorage)
+            localStorage.setItem("datosJSON", JSON.stringify(data));
+
+            // Redirigir al usuario al archivo resultado.html
+            window.location.href = "Ficha.html";
         })
         .catch(error => {
-            console.error('Error al cargar el archivo JSON:', error);
+            console.error("Error:", error);
         });
-    
-    window.location.href = "Ficha.html";
 
     Swal.close();
 }
