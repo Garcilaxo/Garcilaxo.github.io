d9acb7fdf357768588963309ab058a003636dea6
Garcilaxo <alejandrogaf91@gmail.com>
2023-09-13 16:04:20 +0200

    act

--- js/app.js
+++ js/app.js
@@ -44,6 +44,7 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById("slider3").value = `${parseFloat(data.influcard.insight_perc_25.toFixed(2))}`;
       document.getElementById("slider4").value = `${parseFloat(data.influcard.insight_perc_35.toFixed(2))}`;
       document.getElementById("slider5").value = `${parseFloat(data.influcard.insight_perc_45.toFixed(2))}`;
+      document.getElementById("slider6").value = `${parseFloat(data.influcard.insight_perc_65.toFixed(2))}`;
 
       document.getElementById("ES").textContent = `${data.influcard.top_countries_formated[0].country_short}`;
       document.getElementById("US").textContent = `${data.influcard.top_countries_formated[0].country_short}`;
